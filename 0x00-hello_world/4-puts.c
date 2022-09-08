fe52937fda785cf7f17681cef4069835e2b4cf38
Sarah <tadekolasarah@yahoo.com>
2022-09-08 16:21:44 -0700

    task 4 amended

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
-/* a program that prints "Programming is like building a multilingual puzzle */
-int main (void)
+#include <string.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
