6dd3468d9111b1b7e8f18b894b3c36781b47b6e5
Sarah <tadekolasarah@yahoo.com>
2022-09-09 18:51:17 -0700

    task 10 added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 57; x==)
+	{
+		for (y = 48; y <= 57; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (x != 57 || y != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
