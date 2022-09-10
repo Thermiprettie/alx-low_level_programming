c1ede39d1f54a5ec342222b085a9923dd68a4dcb
Sarah <tadekolasarah@yahoo.com>
2022-09-09 18:14:42 -0700

    task 8 correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *
  * main -Entry point
  *
  * Return: Always 0 (Success)
- *
  */
-
 int main(void)
+{
 	int m;
 	char st;
 
-	for (m = '0'; m <= '9'; m++)
-{
-	putchar(m);
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+	}
 	for (st = 'a'; st <= 'f'; st++)
+	{
 		putchar(st);
+	}
 	putchar('\n');
-
 	return (0);
 }
