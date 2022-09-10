47107e66703c2ae9141f4cde07331497125f14bc
Sarah <tadekolasarah@yahoo.com>
2022-09-09 18:02:02 -0700

    adjusted task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,12 @@
  */
 int main(void)
 {
-	char st;
+	int st;
 
 	for (st = 'a'; st <= 'z'; st++)
 		putchar(st);
 	for (st = 'A'; st <= 'Z'; st++)
 		putchar(st);
-		putchar('\n');
-
+	putchar('\n');
 	return (0);
 }
