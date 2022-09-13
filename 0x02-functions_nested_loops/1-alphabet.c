a47c8496d425e5a0ff5aadcb5f5e18afe3f922d2
Sarah <tadekolasarah@yahoo.com>
2022-09-13 10:38:30 -0700

    adding task 0 and 1 files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print alphabet in lower case
+ * followed by a new line
+ * Return 0
+ */
+void print_alphabet(void)
+{
+	char saralphabet;
+
+	for (saralphabet = 'a'; saralphabet <= 'z'; saralphabet++)
+	{
+		_putchar(saralphabet);
+	}
+	_putchar('\n');
+}
