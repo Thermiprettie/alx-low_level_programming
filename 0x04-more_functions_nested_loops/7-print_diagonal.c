6d3b4cfd41b63b05a3722b45686cd3ec2d452a9f
Sarah <tadekolasarah@yahoo.com>
2022-09-15 09:05:47 -0700

    edited

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,12 +3,12 @@
 /**
  * print_diagonal - a function that draws a diagonal
  * line on the terminal
+ * @n: number of times '\' should be printed
  * character \ should be printed n times
  * The diagonal will end with a \n
  * print only \n if n is 0 or less
  * Return: diagonal line
  */
-
 void print_diagonal(int n)
 {
 	int lin;
