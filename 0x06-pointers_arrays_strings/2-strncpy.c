c4cfba4dac6511ae3cd7965ebe19801dba0c1eca
Sarah <tadekolasarah@yahoo.com>
2022-09-22 09:46:18 -0700

    adding task 2 again

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,3 +8,18 @@
  * Return: Always 0
  */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int ats = 0, byo = 0;
+
+	while (src[ats++])
+		byo++;
+
+	for (ats = 0; src[ats] && ats < n; ats++)
+		dest[ats] = src[ats];
+
+	for (ats = byo; ats < n; ats++)
+		dest[ats] = '\0';
+
+	return (dest);
+}
