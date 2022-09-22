bdfc1a61d926152f3c9c5c2fa70693d1d875d52d
Sarah <tadekolasarah@yahoo.com>
2022-09-22 09:40:11 -0700

    task 2 file deleted

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,10 @@
 #include "main.h"
+
 /**
- * _strncpy - function that copies a string
+ * _strncpy - copy a string
  * @dest: destination
- * @src: source
- * @n: integer
- * Return: copied string
+ * @src: source to copy from
+ * @n: input
+ * Return: Always 0
  */
-char *_strncpy(char *dest, char *src, int n)
-{
-int ats = 0, byo = 0;
 
-while (src[ats++])
-	byo++;
-	
-for (ats = 0; src[ats] && ats < n; ats++)
-		dest[ats] = src[ats];
-
-for (ats = byo; ats < n; ats++)
-		dest[ats] = '\0';
-
-return (dest);
-}
