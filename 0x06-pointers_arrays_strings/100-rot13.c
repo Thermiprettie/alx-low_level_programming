6f5708e81c02c042cc52079f3f044dfbb9de4208
Sarah <tadekolasarah@yahoo.com>
2022-09-22 11:04:15 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * rot 13 - encodes a string
+ * @estr: string to encode
+ * Return: encoded string
+ */
+
+char *rot13(char *estr)
+{
+	int aa = 0, ab;
+	char letters[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
+
+	while (estr[aa])
+	{
+		for (ab = 0; ab < 52; ab++)
+		{
+			if (estr[aa] == letters[ab])
+			{
+				estr[aa] = rot13key[ab];
+				break;
+			}
+		}
+
+		aa++;
+	}
+
+	return (estr);
+}
