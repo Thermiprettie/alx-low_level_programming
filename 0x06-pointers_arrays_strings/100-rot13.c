74272ed3f9cd6b00c70b169e60eb41e4ca1fe42f
Sarah <tadekolasarah@yahoo.com>
2022-09-22 11:43:17 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,8 +8,7 @@
 char *rot13(char *estr)
 {
 	int aa = 0, ab;
-	char letters[52] =
-	{'A', 'B', 'C', 'D', 'E', 'F',
+	char letters[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 		'G', 'H', 'I', 'J', 'K', 'L',
 		'M', 'N', 'O', 'P', 'Q', 'R',
 		'S', 'T', 'U', 'V', 'W', 'X',
@@ -41,6 +40,5 @@ char *rot13(char *estr)
 
 		aa++;
 	}
-
 	return (estr);
 }
