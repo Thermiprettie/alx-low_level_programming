8904422dec46ef68e4d5bd44ea67156ce1b8278a
Sarah <tadekolasarah@yahoo.com>
2022-10-04 07:13:33 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of character, initialising a specific character
+ *
+ * @size: size of the array
+ * @c: character
+ *
+ * Return: array of character
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int u;
+	char *s;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	s = malloc(size * sizeof(char));
+
+	if (size == NULL)
+	{
+		return (NULL);
+	}
+
+	for (u = 0; u < size; u++)
+	{
+		s[u] = c;
+	}
+
+	return (s);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - main.h file
+ */
+
+char *create_array(unsigned int size, char c);
+
+#endif
