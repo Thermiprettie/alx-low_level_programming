f0c3059364bbf5b73582332491e63fb4e0df235a
Sarah <tadekolasarah@yahoo.com>
2022-10-08 07:18:30 -0700

    betty error resolved

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (id = 0; s1[id]; id++)
 		len++;
 
-	concate = malloc(sizeof(char) * (len +1));
+	concate = malloc(sizeof(char) * (len + 1));
 
 	if (concate == NULL)
 		return (NULL);
