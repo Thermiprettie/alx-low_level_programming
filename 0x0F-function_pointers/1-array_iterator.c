00c5835586d4a7b378fdab4405737eb07170c95a
Sarah <tadekolasarah@yahoo.com>
2022-10-12 13:43:46 -0700

    task 1 again

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: array
+ * @size: size of array
+ * @action: pointer to funtion
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+
+}
