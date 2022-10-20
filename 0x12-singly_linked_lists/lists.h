f2a4edc790ef4c1e197e6217b10696a47aca362c
Thermiprettie <tadekolasarah@yahoo.com>
2022-10-20 14:26:53 -0700

    main h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,6 @@
 
 #include <string.h>
 #include <stdlib.h>
-#include <stdargs.h>
 #include <stdio.h>
 
 /**
@@ -21,16 +20,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* prototypes */
-
-size_t list_len(const list_t *h);
-
-list_t *add_node(list_t **head, const char *str);
-
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-
 void free_list(list_t *head);
 
 #endif
