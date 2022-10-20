abcad1f648770e5e2bfdc46f20efa6e22265a713
Thermiprettie <tadekolasarah@yahoo.com>
2022-10-20 14:15:46 -0700

    an update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,21 @@
 #include <stdargs.h>
 #include <stdio.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 /* prototypes */
 
 size_t list_len(const list_t *h);
