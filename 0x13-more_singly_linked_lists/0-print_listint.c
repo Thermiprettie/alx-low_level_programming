0209d683d5c68f332b5dff570a9ae7bd0979ffbe
Thermiprettie <tadekolasarah@yahoo.com>
2022-10-25 04:20:14 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t list
+ * Return: elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodess = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodess++;
+	}
+	return (counter);
+}
