955e2f3c2ef01e9bad6d356da7e7da46e81462dc
Thermiprettie <tadekolasarah@yahoo.com>
2022-10-27 14:04:17 -0700

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the no of bits thant is to be flipped
+ * to get from one number to another
+ * @n: number
+ * @m: another number
+ * Return: number of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int set, lukup;
+	unsigned int counter, r;
+
+	counter = 0;
+	lukup = 1;
+	set = n ^ m;
+	for (r = 0; r < (sizeof(unsigned long int) * 8); r++)
+	{
+		if (lukup == (set & lukup))
+			counter++;
+		lukup <<= 1;
+	}
+	return(counter);
+}
